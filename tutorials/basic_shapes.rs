53f63eb8135859d93b9dce93ebbdb29e52639723
Seth Boyles <sethboyles@gmail.com>
2019-10-23 20:12:05 -0700

    ob
    Authored-by: Seth Boyles <sethboyles@gmail.com>

--- tutorials/basic_shapes.rs
+++ tutorials/basic_shapes.rs
@@ -1,29 +1,38 @@
 use nannou::prelude::*;
-
 fn main() {
-    nannou::sketch(view);
+    nannou::app(model).run();
 }
 
-fn view(app: &App, frame: &Frame) {
+struct Model {
+    // Store the window ID so we can refer to this specific window later if needed.
+}
+
+fn model(app: &App) -> Model {
+    // Create a new window! Store the ID so we can refer to it later.
+    let _window = app
+        .new_window()
+        .with_dimensions(956, 520)
+        .with_title("nannou")
+        .view(view) // The function that will be called for presenting graphics to a frame.
+        .build()
+        .unwrap();
+    Model { }
+}
+
+fn view(app: &App, _model: &Model, frame: &Frame) {
     // Begin drawing
-    let draw = app.draw();
+    let draw = app
+        .draw();
+    let start_point = pt2(-30.0, -20.0);
+let end_point   = pt2(40.0, 40.0);
 
-    // Clear the background.
+draw.line()
+    .start(start_point)
+    .end(end_point)
+    .weight(4.0)
+    .color(STEELBLUE);
+       // Clear the background.
     draw.background().color(PLUM);
+    draw.to_frame(app, &frame).unwrap();
 
-
-    
-    let point1 = pt2(-10.0, -20.0);
-    let point2 = pt2(10.0, -30.0);
-    let point3 = pt2(15.0, 40.0);
-    let point4 = pt2(-20.0, 35.0);
-
-    draw.quad()
-        .color(STEELBLUE)
-        .w(300.0)
-        .h(200.0)
-        .points(point1, point2, point3, point4); 
-
-    // Write to the window frame.
-    draw.to_frame(app, &frame).unwrap(); 
 }
